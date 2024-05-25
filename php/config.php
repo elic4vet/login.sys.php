60bf6acc7d28cc7456d8e635002b2fd9a8ba6874
Elisabeth <elise.doisneau@gmail.com>
2024-05-25 19:46:51 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,14 @@
 <?php 
- 
- $con = mysqli_connect("localhost","root","","login") or die("Couldn't connect");
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = 'login';
+
+$conn = mysqli_connect($host, $user, $pass, $db) or die("Could not connect to database");
+
+
+echo "Connected to database";
 
 ?>
+ 
